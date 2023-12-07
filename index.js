14629c3ef153dbdb4565e1973f17f562b824cc6e
Rasmus Bremholm <rasmus.brem@gmail.com>
2023-12-07 18:54:48 +0100

    Create API endpoints

--- index.js
+++ index.js
@@ -24,4 +24,80 @@ const legions = [
 		primarch: "Fulgrim",
 		traitor: true,
 	},
+	{
+		id: 4,
+		name: "Iron Warriors",
+		description: "...",
+		primarch: "Perturabo",
+		traitor: true,
+	},
+	{
+		id: 5,
+		name: "White Scars",
+		description: "...",
+		primarch: "Jaghatai Khan",
+		traitor: false,
+	},
+	{
+		id: 6,
+		name: "Space Wolves",
+		description: "...",
+		primarch: "Leman Russ",
+		traitor: false,
+	},
+	{
+		id: 7,
+		name: "Imperial Fists",
+		description: "...",
+		primarch: "Rogal Dorn",
+		traitor: false,
+	},
+	{
+		id: 8,
+		name: "Night Lords",
+		description: "...",
+		primarch: "Konrad Curze",
+		traitor: true,
+	},
+	{
+		id: 9,
+		name: "Blood Angels",
+		description: "...",
+		primarch: "Sanguinius",
+		traitor: false,
+	},
+	{
+		id: 10,
+		name: "Iron Hands",
+		description: "...",
+		primarch: "Ferrus Manus",
+		traitor: false,
+	},
+	{
+		id: 11,
+		name: "[ACESS DENIED]",
+		description: "...",
+		primarch: "[...REDACTED...]",
+		traitor: true,
+	},
 ];
+
+// Route to all legions.
+app.get("/legions", (req, res) => {
+	res.json(legions);
+});
+
+// Route to specific legion
+app.get("/factions/:id", (req, res) => {
+	const legion = legions.find((f) => f.id === parseInt(req.params.id));
+
+	if (legion) {
+		res.json(legion);
+	} else {
+		res.status(404).send("Legion not found");
+	}
+});
+
+app.listen(port, () => {
+	console.log(`Server running on port: ${port}`);
+});
