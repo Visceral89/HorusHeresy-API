e57ad49b7e27789e247ee351ecb0ddc2c0e25bd4
Rasmus Bremholm <rasmus.brem@gmail.com>
2023-12-12 19:14:27 +0100

    Caching
    CACHING IS FUCKING DONE BABYYYY
    On to testing!

--- index.js
+++ index.js
@@ -1,130 +1,56 @@
 const express = require("express");
 const app = express();
 const port = 1337;
+const NodeCache = require("node-cache");
 import pool from "./database.js";
 
-/* OLD ARRAY THAT I DONT USE
-const legions = [
-	{
-		id: 1,
-		name: "Dark Angels",
-		description: "...",
-		primarch: "Lion el Johnsson",
-		traitor: false,
-		homeworld: "Caliban",
-		colors: {
-			primary: "#004225",
-			secondary: "#FFFFFF",
-			accent: "#000000",
-		},
-	},
-	{
-		id: 2,
-		name: "[REDACTED]",
-		description: "[...REDACTED...]",
-		primarch: "N/A",
-		traitor: true,
-		homeworld: "N/A",
-	},
-	{
-		id: 3,
-		name: "Emperor's Children",
-		description: "...",
-		primarch: "Fulgrim",
-		traitor: true,
-		homeworld: "Chemos",
-	},
-	{
-		id: 4,
-		name: "Iron Warriors",
-		description: "...",
-		primarch: "Perturabo",
-		traitor: true,
-		homeworld: "Olympia",
-	},
-	{
-		id: 5,
-		name: "White Scars",
-		description: "...",
-		primarch: "Jaghatai Khan",
-		traitor: false,
-		homeworld: "Mundus Planus",
-	},
-	{
-		id: 6,
-		name: "Space Wolves",
-		description: "...",
-		primarch: "Leman Russ",
-		traitor: false,
-		homeworld: "Fenris",
-	},
-	{
-		id: 7,
-		name: "Imperial Fists",
-		description: "...",
-		primarch: "Rogal Dorn",
-		traitor: false,
-	},
-	{
-		id: 8,
-		name: "Night Lords",
-		description: "...",
-		primarch: "Konrad Curze",
-		traitor: true,
-	},
-	{
-		id: 9,
-		name: "Blood Angels",
-		description: "...",
-		primarch: "Sanguinius",
-		traitor: false,
-	},
-	{
-		id: 10,
-		name: "Iron Hands",
-		description: "...",
-		primarch: "Ferrus Manus",
-		traitor: false,
-	},
-	{
-		id: 11,
-		name: "[ACESS DENIED]",
-		description: "...",
-		primarch: "[...REDACTED...]",
-		traitor: true,
-	},
-];
-*/
+const cache = new NodeCache({ stdTTL: 3600, checkperiod: 600 });
 
 // Route to all legions.
 app.get("/legions", async (req, res) => {
-	try {
-		const { rows: legions } = await pool.query("SELECT * FROM legions");
-		res.json(legions);
-	} catch (err) {
-		res.status(500).send(err.message);
+	const cacheKey = "allLegions";
+	const cachedAllLegions = cache.get(cacheKey);
+
+	if (cachedAllLegions) {
+		return res.json(cachedAllLegions);
+	} else {
+		try {
+			const { rows: legions } = await pool.query("SELECT * FROM legions");
+			cache.set(cacheKey, legions);
+			res.json(legions);
+		} catch (err) {
+			res.status(500).send(err.message);
+		}
 	}
 });
 
 // Route to specific legion
 app.get("/legion/:id", async (req, res) => {
-	try {
-		const legionId = parseInt(req.params.id);
-		const query = "SELECT * FROM legions WHERE id = $1";
-		const { rows: legion } = await pool.query(query, [legionId]);
+	const legionId = parseInt(req.params.id);
+	const legionKey = `legion-${legionId}`;
+	const cachedLegion = cache.get(legionKey);
 
-		if (legion.length) {
-			res.json(legion[0]);
-		} else {
-			res.status(404).send("Legion not found, try 1-20");
+	if (cachedLegion) {
+		return res.json(cachedLegion);
+	} else {
+		try {
+			const query = "SELECT * FROM legions WHERE id=$1";
+			const { rows: legion } = await pool.query(query, [legionId]);
+
+			if (legion.length) {
+				cache.set(legionKey, legion[0]);
+			} else {
+				res.status(404).send("Legion not found, try 1-20");
+			}
+		} catch (err) {
+			console.error(err.message);
+			res.status(500).send("Error to the sooorvooor");
 		}
-	} catch (err) {
-		console.error(err.message);
-		res.status(500).send("Error to the sooorvooor");
 	}
 });
 
-//Query route to Traitor Legions
+//Query route to Traitor Legions (old, remove after testing)
+/*
 app.get("/legions/traitors", async (req, res) => {
 	try {
 		const query = "SELECT * FROM legions WHERE traitor = true";
@@ -135,6 +61,26 @@ app.get("/legions/traitors", async (req, res) => {
 		res.status(500).send("Erroor to tha soorvoor");
 	}
 });
+*/
+app.get("/legions/traitors", async (req, res) => {
+	const traitorKey = "traitorLegions";
+	const cachedTraitors = cache.get(traitorKey);
+
+	if (cachedTraitors) {
+		return res.json(cachedTraitors);
+	} else {
+		try {
+			const query = "SELECT * FROM legions WHERE traitor = true";
+			const { rows: traitorLegions } = await pool.query(query);
+			cache.set(traitorKey, traitorLegions);
+			res.json(traitorLegions);
+		} catch (err) {
+			console.error(err.message);
+			res.status(500).send("We need moore detitated wam to da sooorvor");
+		}
+	}
+});
+/*
 app.get("/legions/loyalist", async (req, res) => {
 	try {
 		const query = "SELECT * FROM legions WHERE traitor = false";
@@ -143,6 +89,25 @@ app.get("/legions/loyalist", async (req, res) => {
 	} catch (err) {
 		console.error(err.message);
 		res.status(500).send("Erroor to tha soorvoor");
+	}
+});
+*/
+app.get("/legions/loyalist", async (req, res) => {
+	const loyalistKey = "loyalistLegions";
+	const cachedLoyalists = cache.get(loyalistKey);
+
+	if (cachedLoyalists) {
+		return res.json(cachedLoyalists);
+	} else {
+		try {
+			const query = "SELECT * FROM legions WHERE traitor = false";
+			const { rows: loyalistLegions } = await pool.query(query);
+			cache.set(loyalistKey, loyalistLegions);
+			res.json(loyalistLegions);
+		} catch (err) {
+			console.error(err.message);
+			res.status(500).send("We need moore detitated wam to da sooorvor");
+		}
 	}
 });
 
